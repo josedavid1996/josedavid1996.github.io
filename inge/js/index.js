f370b94cb241085e944613de032793ca59f7f8e2
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2021-07-29 20:12:18 -0500

    Guarde todo

--- inge/js/index.js
+++ inge/js/index.js
@@ -1,5 +1,5 @@
 import { datos } from './datos.js';
-console.log(datos.spark['presion-ti'])
+// console.log(datos.spark['presion-ti'])
 
 const d = document;
 
@@ -11,32 +11,19 @@ const $pintar = d.querySelector('.pintar')
 d.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = d.querySelector("#text").value;
-  if (text === 'spark') {
-    $template.querySelector("h1").textContent = datos.spark.title
-    $template.querySelector("img").setAttribute("src", datos.spark.imagen);
-    $template.querySelectorAll("h2")[0].textContent = `Presion DD = ${datos.spark.presiondd} `
-    $template.querySelectorAll("h2")[1].textContent = `Presion DI = ${datos.spark.presiondi} `;
-    $template.querySelectorAll("h2")[2].textContent = `Presion TD = ${datos.spark.presiontd} `;
-    $template.querySelectorAll("h2")[3].textContent = `Presion TI = ${datos.spark.presionti} `;
-    $template.querySelectorAll("h2")[4].textContent = `Presion DD = ${datos.spark.carroseria} `;
-    $template.querySelectorAll("h2")[5].textContent = `Presion DD = ${datos.spark.servicio} `;
-    ;
+  console.log(text)
+  $template.querySelector("h1").textContent = datos[text].title
+  $template.querySelector("img").setAttribute("src", datos[text].imagen);
+  $template.querySelectorAll("h2")[0].textContent = `Presion DD = ${datos[text].presiondd} `
+  $template.querySelectorAll("h2")[1].textContent = `Presion DI = ${datos[text].presiondi} `;
+  $template.querySelectorAll("h2")[2].textContent = `Presion TD = ${datos[text].presiontd} `;
+  $template.querySelectorAll("h2")[3].textContent = `Presion TI = ${datos[text].presionti} `;
+  $template.querySelectorAll("h2")[4].textContent = `Carroseria = ${datos[text].carroseria} `;
+  $template.querySelectorAll("h2")[5].textContent = `Servicio = ${datos[text].servicio} `;
+  ;
 
-    let $clone = document.importNode($template, true);
-    $fragment.appendChild($clone);
-
-  } else if (text === 'hilux') {
-    $template.querySelector("h1").textContent = datos.hilux.title
-    $template.querySelector("img").setAttribute("src", datos.hilux.imagen);
-    $template.querySelectorAll("h2")[0].textContent = `Presion DD = ${datos.hilux.presiondd} `
-    $template.querySelectorAll("h2")[1].textContent = `Presion DI = ${datos.hilux.presiondi} `;
-    $template.querySelectorAll("h2")[2].textContent = `Presion TD = ${datos.hilux.presiontd} `;
-    $template.querySelectorAll("h2")[3].textContent = `Presion TI = ${datos.hilux.presionti} `;
-    $template.querySelectorAll("h2")[4].textContent = `Presion DD = ${datos.hilux.carroseria} `;
-    $template.querySelectorAll("h2")[5].textContent = `Presion DD = ${datos.hilux.servicio} `;
-    let $clone = document.importNode($template, true);
-    $fragment.appendChild($clone);
-  }
+  let $clone = document.importNode($template, true);
+  $fragment.appendChild($clone);
   form.reset();
   $pintar.appendChild($fragment);
 
