a9e80aed254c5140a2e2111a9ecec69338cc945f
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2021-07-31 18:51:20 -0500

    Agregue la pwa

--- buscador/js/index.js
+++ buscador/js/index.js
@@ -33,3 +33,8 @@ d.addEventListener('submit', (e) => {
 // d.addEventListener('click', (e) => {
 //   console.log(e.target)
 // })
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
