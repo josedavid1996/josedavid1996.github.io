08e78d627285e35eb99dc8a0b555f8004d0a8e24
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2021-07-31 20:18:34 -0500

    borre la pwa

--- buscador/js/index.js
+++ buscador/js/index.js
@@ -36,5 +36,5 @@ d.addEventListener('submit', (e) => {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+    .catch(err => console.warn('Error  del sw', err))
 }
